2e1545fc0b5f1dbc1efcac549d8b68e6754e2d8e
ayazimahsa <mahsaayazii73@gmail.com>
2022-06-22 20:37:02 +0430

    add hw5

--- hw4/script.js
+++
@@ -1,6 +0,0 @@
-let lis = document.getElementsByClassName('item');
-for (let i = 0; i < lis.length; i++) {
-    lis[i].addEventListener('click', () => {
-        lis[i].style.textDecoration = "line-through"
-    });
-}
---
+++ hw5/script.js
@@ -0,0 +1,10 @@
+let buttons = document.querySelector("#buttons");
+let button = document.querySelectorAll('button');
+
+buttons.addEventListener("click", (event) => {
+    let clicked = event.target;
+    if ((clicked.className === "buttonClass"))
+        console.log("buttonClass");
+    else if ((clicked.className === "buttonClass2"))
+        console.log("click");
+});
