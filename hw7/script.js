e334d944fc241378d6071444bff1252d566e35c5
ayazimahsa <mahsaayazii73@gmail.com>
2022-06-22 20:39:06 +0430

    add hw7

--- hw7/script.js
+++ hw7/script.js
@@ -1,15 +1,10 @@
-//1
-let h1 = document.getElementById('elemId');
-h1.addEventListener('click', () => {
-    console.log('goodBye');
-
-});
-
-//2
-function showCoords(event) {
+let span = document.getElementById('show_position');
+function event_function(event){
     let x = event.clientX;
     let y = event.clientY;
     let Coordinates = "X Coordinates: " + x + ", Y Coordinates: " + y;
     console.log(Coordinates);
+    span.innerText=Coordinates;
+    console.log(span);
 }
 
